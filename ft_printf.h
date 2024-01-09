d9069414cd1990f161d1b75e04ab258aa92ddb16
Abdelali <abde69436@gmail.com>
2024-01-09 13:34:24 +0100

    header

--- ft_printf.h
+++ ft_printf.h
@@ -6,7 +6,7 @@
 /*   By: abmahfou <abmahfou@student.1337.ma>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/01/05 16:09:46 by abmahfou          #+#    #+#             */
-/*   Updated: 2024/01/08 11:52:31 by abmahfou         ###   ########.fr       */
+/*   Updated: 2024/01/09 13:29:47 by abmahfou         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,7 +17,7 @@
 # include <unistd.h>
 # include <stdlib.h>
 
-int	ft_print_num(int num);
+int	ft_print_digit(long num, int base, char s);
 int	ft_print_str(const char *str);
 
 #endif
