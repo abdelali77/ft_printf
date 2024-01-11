7961eeb17be664da845aaa29a92bac0b42c21352
Abdelali <abde69436@gmail.com>
2024-01-11 11:31:01 +0100

    add header

--- ft_printf.h
+++ ft_printf.h
@@ -6,7 +6,7 @@
 /*   By: abmahfou <abmahfou@student.1337.ma>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/01/05 16:09:46 by abmahfou          #+#    #+#             */
-/*   Updated: 2024/01/09 13:29:47 by abmahfou         ###   ########.fr       */
+/*   Updated: 2024/01/11 11:12:14 by abmahfou         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,8 +16,12 @@
 # include <stdarg.h>
 # include <unistd.h>
 # include <stdlib.h>
+# include <stdio.h>
 
-int	ft_print_digit(long num, int base, char s);
+int	ft_print_dec_hex(long num, int base, char s);
 int	ft_print_str(const char *str);
+int	ft_print_addr(void	*p);
+int	ft_print_unsigned(unsigned int n);
+int	ft_print_char(int c);
 
 #endif
