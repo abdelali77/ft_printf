8b976903c66bbe8d12192a79ec728546555e5b18
Abdelali <abde69436@gmail.com>
2024-01-11 11:30:45 +0100

    main

--- ft_printf.c
+++ ft_printf.c
@@ -6,16 +6,11 @@
 /*   By: abmahfou <abmahfou@student.1337.ma>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/01/05 21:26:40 by abmahfou          #+#    #+#             */
-/*   Updated: 2024/01/09 13:31:00 by abmahfou         ###   ########.fr       */
+/*   Updated: 2024/01/11 11:14:52 by abmahfou         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ft_printf.h"
-
-static int	ft_print_char(char c)
-{
-	return (write(1, &c, 1));
-}
 
 static int	print_format(char s, va_list ap)
 {
@@ -25,19 +20,21 @@ static int	print_format(char s, va_list ap)
 	if (s == 'c')
 		n += ft_print_char(va_arg(ap, int));
 	else if (s == 'd' || s == 'i')
-		n += ft_print_digit((long)va_arg(ap, int), 10, s);
+		n += ft_print_dec_hex((long)va_arg(ap, int), 10, s);
 	else if (s == 's')
 		n += ft_print_str(va_arg(ap, char *));
 	else if (s == '%')
-		n += write(1, "%", 1);
-	// else if (s == 'p')
-	// 	n += ft_print_addr(va_arg(ap, char *));
+		n += ft_print_char('%');
+	else if (s == 'p')
+		n += ft_print_addr(va_arg(ap, void *));
 	else if (s == 'x')
-		n += ft_print_digit((long)(va_arg(ap, unsigned int)), 16, s);
+		n += ft_print_dec_hex((long)(va_arg(ap, unsigned int)), 16, s);
 	else if (s == 'X')
-		n += ft_print_digit((long)(va_arg(ap, unsigned int)), 16, s);
+		n += ft_print_dec_hex((long)(va_arg(ap, unsigned int)), 16, s);
+	else if (s == 'u')
+		n += ft_print_unsigned(va_arg(ap, unsigned int));
 	else
-		return (write(1, &s, 1));
+		return (ft_print_char(s));
 	return (n);
 	va_end(ap);
 }
@@ -54,19 +51,27 @@ int	ft_printf(const char *format, ...)
 	while (format[i])
 	{
 		if (format[i] == '%')
+		{
 			char_printed += print_format(format[++i], args);
+			if (char_printed < 0)
+				return (-1);
+		}
 		else
+		{
 			char_printed += ft_print_char(format[i]);
+			if (char_printed < 0)
+				return (-1);
+		}
 		i++;
 	}
 	va_end(args);
 	return (char_printed);
 }
 
-/* #include <stdio.h>
+#include <stdio.h>
 int main()
 {
-	//char *str = "hello";
-	printf("%X\n", 454646);
-	ft_printf("%X\n", 454646);
-} */
+	int arr[] = {10, 15, 17};
+	printf("%p\n", arr);
+	ft_printf("%p\n", arr);
+}
