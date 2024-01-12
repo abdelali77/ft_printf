fa8383a5a6747d981e051074887475ce18a9de82
Abdelali <abde69436@gmail.com>
2024-01-12 11:51:32 +0100

    main

--- ft_printf.c
+++ ft_printf.c
@@ -6,13 +6,13 @@
 /*   By: abmahfou <abmahfou@student.1337.ma>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/01/05 21:26:40 by abmahfou          #+#    #+#             */
-/*   Updated: 2024/01/11 11:14:52 by abmahfou         ###   ########.fr       */
+/*   Updated: 2024/01/12 11:47:00 by abmahfou         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ft_printf.h"
 
-static int	print_format(char s, va_list ap)
+int	print_format(char s, va_list ap)
 {
 	int		n;
 
@@ -27,9 +27,7 @@ static int	print_format(char s, va_list ap)
 		n += ft_print_char('%');
 	else if (s == 'p')
 		n += ft_print_addr(va_arg(ap, void *));
-	else if (s == 'x')
-		n += ft_print_dec_hex((long)(va_arg(ap, unsigned int)), 16, s);
-	else if (s == 'X')
+	else if (s == 'x' || s == 'X')
 		n += ft_print_dec_hex((long)(va_arg(ap, unsigned int)), 16, s);
 	else if (s == 'u')
 		n += ft_print_unsigned(va_arg(ap, unsigned int));
@@ -51,27 +49,22 @@ int	ft_printf(const char *format, ...)
 	while (format[i])
 	{
 		if (format[i] == '%')
-		{
 			char_printed += print_format(format[++i], args);
-			if (char_printed < 0)
-				return (-1);
-		}
 		else
-		{
 			char_printed += ft_print_char(format[i]);
-			if (char_printed < 0)
-				return (-1);
-		}
+		if (char_printed < 0)
+			return (-1);
 		i++;
 	}
 	va_end(args);
 	return (char_printed);
 }
 
-#include <stdio.h>
+/* #include <stdio.h>
+#include <limits.h>
+#include <fcntl.h>
 int main()
 {
-	int arr[] = {10, 15, 17};
-	printf("%p\n", arr);
-	ft_printf("%p\n", arr);
-}
+	ft_printf("%+d\n", 42);
+	printf("% d\n", -42);
+} */
